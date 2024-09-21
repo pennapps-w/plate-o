5a7b785de04810f514a54da3819949e6dc2febbd
blobotic <vli3@andrew.cmu.edu>
2024-09-21 12:18:47 -0400

    restaurants into db + recommender

---
+++ project1/add_restaurants.py
@@ -0,0 +1,45 @@
+from requests import get, post, put, delete, HTTPError
+import json 
+
+
+jd = None 
+
+with open("../merged_restaurant_data.json") as f:
+    jd = json.load(f)
+
+
+
+def test_api():
+    """
+    An automated version of the manual testing I've been doing,
+    testing the lifecycle of an inserted document.
+    """
+    user_root = "http://127.0.0.1:8000/restaurants/"
+
+    try:
+        # Insert a user
+        for i in jd:
+            print(i)
+            response = post(user_root, json={"data": i})
+            response.raise_for_status()
+            doc = response.json()
+            print(doc)
+            inserted_id = doc["id"]
+            print(inserted_id)
+            print(f"Inserted document with id: {inserted_id}")
+            print(
+                "If the test fails in the middle you may want to manually remove the document."
+            )
+
+            # List users and ensure it's present
+            # response = get(user_root)
+            # response.raise_for_status()
+            # user_ids = {s["id"] for s in response.json()["users"]}
+
+
+        
+    except HTTPError as he:
+        print(he.response.json())
+        raise
+
+test_api()
